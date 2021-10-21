56f891631d5b5b2f0d746ef12c01742e3798998a
StephUbaud <92312963+StephUbaud@users.noreply.github.com>
2021-10-21 09:35:51 +0200

    Mise à jour pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,33 @@
-serial.write_value("x", input.magnetic_force(Dimension.X))
-serial.write_value("y", input.magnetic_force(Dimension.Y))
-serial.write_value("z", input.magnetic_force(Dimension.Z))
+direction = 0
+
+def on_button_pressed_a():
+    input.calibrate_compass()
+input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_forever():
-    input.calibrate_compass()
+    serial.write_value("x", input.magnetic_force(Dimension.X))
+    serial.write_value("y", input.magnetic_force(Dimension.Y))
+    serial.write_value("z", input.magnetic_force(Dimension.Z))
+    serial.write_value("angle", input.compass_heading())
 basic.forever(on_forever)
+
+def on_forever2():
+    global direction
+    direction = input.compass_heading()
+    if direction > 337.5 or direction <= 22.5:
+        basic.show_arrow(ArrowNames.NORTH)
+    if direction > 22.5 and direction <= 67.5:
+        basic.show_arrow(ArrowNames.NORTH_EAST)
+    if direction > 67.5 and direction <= 112.5:
+        basic.show_arrow(ArrowNames.EAST)
+    if direction > 112.5 and direction <= 157.5:
+        basic.show_arrow(ArrowNames.SOUTH_EAST)
+    if direction > 157.5 and direction <= 202.5:
+        basic.show_arrow(ArrowNames.SOUTH)
+    if direction > 202.5 and direction <= 247.5:
+        basic.show_arrow(ArrowNames.SOUTH_WEST)
+    if direction > 247.5 and direction <= 292.5:
+        basic.show_arrow(ArrowNames.WEST)
+    if direction > 292.5 and direction <= 337.5:
+        basic.show_arrow(ArrowNames.NORTH_WEST)
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,19 +1,46 @@
 let direction = 0
-input.calibrateCompass()
-basic.forever(function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
+    input.calibrateCompass()
+})
+basic.forever(function on_forever() {
     serial.writeValue("x", input.magneticForce(Dimension.X))
     serial.writeValue("y", input.magneticForce(Dimension.Y))
     serial.writeValue("z", input.magneticForce(Dimension.Z))
+    serial.writeValue("angle", input.compassHeading())
 })
-basic.forever(function () {
+basic.forever(function on_forever2() {
+    
     direction = input.compassHeading()
-    if (direction > 337.5 && direction < 22.5) {
+    if (direction > 337.5 || direction <= 22.5) {
         basic.showArrow(ArrowNames.North)
-    } else if (false) {
-    	
-    } else if (false) {
-    	
-    } else {
-    	
     }
+    
+    if (direction > 22.5 && direction <= 67.5) {
+        basic.showArrow(ArrowNames.NorthEast)
+    }
+    
+    if (direction > 67.5 && direction <= 112.5) {
+        basic.showArrow(ArrowNames.East)
+    }
+    
+    if (direction > 112.5 && direction <= 157.5) {
+        basic.showArrow(ArrowNames.SouthEast)
+    }
+    
+    if (direction > 157.5 && direction <= 202.5) {
+        basic.showArrow(ArrowNames.South)
+    }
+    
+    if (direction > 202.5 && direction <= 247.5) {
+        basic.showArrow(ArrowNames.SouthWest)
+    }
+    
+    if (direction > 247.5 && direction <= 292.5) {
+        basic.showArrow(ArrowNames.West)
+    }
+    
+    if (direction > 292.5 && direction <= 337.5) {
+        basic.showArrow(ArrowNames.NorthWest)
+    }
+    
 })
